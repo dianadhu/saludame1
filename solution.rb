a2f09e0b11c55b59ccb03333bf43fcb3b53b8418
dianadhu <46409024+dianadhu@users.noreply.github.com>
2019-11-04 21:14:57 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,11 +1,14 @@
-require "sinatra"
+require 'sinatra'
 
 get '/' do
-  unless params[:nombre]
-    "¡Hola desconocido!"
-  else
-    <<-HTML
-      <h1>"¡Hola #{params[:nombre]}!"</h1>
-    HTML
-  end
+  if params[:nombre] && params[:nombre]!=""
+		<<-HTML
+		<h1>Hola #{params[:nombre]}!</h1>
+		HTML
+
+	else
+		<<-HTML
+		<h1>Hola desconocido!</h1>
+		HTML
+	end
 end
